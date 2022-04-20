0f95ea1fda02770409ffe9dab05a15d93dfb60dd
kurapoko <kuraharamasahiro@gmail.com>
2022-04-20 15:45:28 +0900

    init scrollMagic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import "./styles.css";
+import ScrollMagic from "scrollmagic";
+import { TweenMax } from "gsap";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const controller = new ScrollMagic.Controller();
+const item = document.getElementById("block2-item");
+const scene = new ScrollMagic.Scene({
+  triggerElement: "#block2",
+  duration: 2500,
+  triggerHook: "onLeave"
+})
+  .setPin("#block2")
+  .addTo(controller);
+
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "#block2",
+  duration: 1250,
+  triggerHook: "onCenter"
+}).on("progress", function (prog) {
+  let p = prog.progress;
+  item
+    .css({
+      bottom: 600 - (600 - p)
+    })
+    .addTo(controller);
+});
