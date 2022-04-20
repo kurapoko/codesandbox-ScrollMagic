b92085128ddd8538baa96c623414caedab37c55d
kurapoko <kuraharamasahiro@gmail.com>
2022-04-21 07:47:21 +0900

    test scrollMagic test scrollMagic

--- src/index.js
+++ src/index.js
@@ -3,24 +3,25 @@ import ScrollMagic from "scrollmagic";
 import { TweenMax } from "gsap";
 
 const controller = new ScrollMagic.Controller();
-const item = document.getElementById("block2-item");
-const scene = new ScrollMagic.Scene({
+const item = document.querySelector("#block2-item");
+
+new ScrollMagic.Scene({
   triggerElement: "#block2",
   duration: 2500,
   triggerHook: "onLeave"
 })
+  .on("progress", function (event) {
+    // console.log(event.progress);
+    console.log(item.style.bottom);
+    item.style.opacity = event.progress * 1;
+  })
   .setPin("#block2")
   .addTo(controller);
 
-const scene2 = new ScrollMagic.Scene({
-  triggerElement: "#block2",
-  duration: 1250,
-  triggerHook: "onCenter"
-}).on("progress", function (prog) {
-  let p = prog.progress;
-  item
-    .css({
-      bottom: 600 - (600 - p)
-    })
-    .addTo(controller);
-});
+// new ScrollMagic.Scene({
+//   triggerElement: "#block2",
+//   duration: 1250,
+//   triggerHook: "onCenter"
+// }).on("progress", function (event) {
+//   console.log("Scene progress changed to " + event.progress);
+// });
